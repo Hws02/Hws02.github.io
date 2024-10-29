02cd7738522c292064f220205e79a71efc69536b
瑞克丁真打代码 <wensheng.huang@qq.com>
2024-10-29 09:55:00 +0800

    Site updated: 2024-10-29 09:54:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/28/hello-world/","2024/10/28/My-New-Post/"];function toRandomPost(){
+var posts=["2024/10/28/My-New-Post/","2024/10/28/hello-world/","2024/10/28/mybatis笔记/","2024/10/28/jdbc 笔记/","2024/10/28/mysql概念笔记/","2024/10/28/Java基础笔记_4_20/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
